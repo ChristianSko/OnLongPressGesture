121e2d743804aa378db897bd0d2ce563a75e4221
Christian Skorobogatow <cskorobogatow@deloitte.es>
2022-07-01 13:36:35 +0200

    Initial Commit

---
+++ OnLongPressGesture/OnLongPressGestureApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnLongPressGestureApp.swift
+//  OnLongPressGesture
+//
+//  Created by Skorobogatow, Christian on 1/7/22.
+//
+
+import SwiftUI
+
+@main
+struct OnLongPressGestureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
